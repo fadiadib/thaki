a723077f249fbf4bda3e4f09c83471b60b46dab3
minalouis-aura <Minaashraf1!>
2021-08-11 13:39:37 +0200

    Minor Modification . Renamed function in violation_car_pane.dart . Removed commented code in violation_car_pane.dart

--- lib/panes/violation/violation_car_pane.dart
+++ lib/panes/violation/violation_car_pane.dart
@@ -16,75 +16,6 @@ import 'package:thaki/widgets/general/section_title.dart';
 class TkViolationCarPane extends TkPane {
   TkViolationCarPane({onDone}) : super(paneTitle: '', onDone: onDone);
 
-  // List<String> _getInitialValuesEN(TkPayer payer) {
-  //   if (payer.selectedCar == null ||
-  //       payer.selectedCar.plateEN == null ||
-  //       payer.selectedCar.plateEN.isEmpty) return ['', ''];
-  //
-  //   final RegExp nExp = RegExp(r"\d{1,4}");
-  //   final String nums = nExp.stringMatch(payer.selectedCar.plateEN);
-  //
-  //   final RegExp cExp = RegExp(r"[A-Z]{2,3}");
-  //   final String chars = cExp.stringMatch(payer.selectedCar.plateEN);
-  //
-  //   return [nums, chars];
-  // }
-
-  // Widget _createForm(TkPayer payer, BuildContext context) {
-  //   int state;
-  //   TkAttributesController states =
-  //       Provider.of<TkAttributesController>(context, listen: true);
-  //   TkLangController langController =
-  //       Provider.of<TkLangController>(context, listen: false);
-  //
-  //   return Column(
-  //     children: [
-  //       // Car license number
-  //       TkSectionTitle(title: S.of(context).kCarPlateEN, uppercase: false),
-  //       TkSectionTitle(title: S.of(context).kCarState, uppercase: false),
-  //       Padding(
-  //         padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
-  //         child: TkDropDownField(
-  //           context: context,
-  //           values: states.stateNames(langController),
-  //           value: states.stateName(state, langController),
-  //           hintText: S.of(context).kCarState,
-  //           onChanged: (value) {
-  //             setState(() => state = states.stateId(value));
-  //           },
-  //           errorMessage: S.of(context).kPleaseChoose + S.of(context).kCarState,
-  //         ),
-  //       ),
-  //       if (payer.selectedCar != null && payer.selectedCar.state != 1)
-  //         Padding(
-  //           padding:
-  //               const EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
-  //           child: TkTextField(
-  //             hintText: S.of(context).kCarPlateEN,
-  //             initialValue: payer.selectedCar.plateEN,
-  //             onChanged: (String value) {
-  //               payer.selectedCar.plateEN = value;
-  //             },
-  //           ),
-  //         ),
-  //       if (payer.selectedCar == null || payer.selectedCar.state == 1)
-  //         Padding(
-  //           padding:
-  //               const EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
-  //           child: TkLicenseField(
-  //             langCode: Provider.of<TkLangController>(context, listen: false)
-  //                 .lang
-  //                 .languageCode,
-  //             onChanged: (value) => payer.selectedCar.plateEN = value,
-  //             values: _getInitialValuesEN(payer),
-  //             validate: false,
-  //             enabled: true,
-  //           ),
-  //         )
-  //     ],
-  //   );
-  // }
-
   Widget _createFormButton(TkPayer payer, BuildContext context) {
     return Padding(
       padding: const EdgeInsetsDirectional.fromSTEB(50.0, 20.0, 50.0, 0),
@@ -124,7 +55,7 @@ class TkViolationCarPane extends TkPane {
                       title: S.of(context).kEnterLRP,
                     ),
                   ),
-                  CreateForm(payer),
+                  TkCreateForm(payer),
                   _createFormButton(payer, context),
                   _getErrorMessage(payer),
                 ],
@@ -134,16 +65,16 @@ class TkViolationCarPane extends TkPane {
   }
 }
 
-class CreateForm extends StatefulWidget {
+class TkCreateForm extends StatefulWidget {
   final TkPayer payer;
 
-  CreateForm(this.payer);
+  TkCreateForm(this.payer);
 
   @override
-  _CreateFormState createState() => _CreateFormState();
+  _TkCreateFormState createState() => _TkCreateFormState();
 }
 
-class _CreateFormState extends State<CreateForm> {
+class _TkCreateFormState extends State<TkCreateForm> {
   TkPayer payer;
 
   List<String> _getInitialValuesEN(TkPayer payer) {
